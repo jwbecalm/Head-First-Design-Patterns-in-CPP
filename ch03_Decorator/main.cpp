e487e825b5d3763381837ae92ccdb77e6995ab14
irwin <17735528@qq.com>
2021-10-26 09:30:21 +0800

    Decorator Pattern, update main.cpp and plantuml

--- ch03_Decorator/main.cpp
+++ ch03_Decorator/main.cpp
@@ -16,6 +16,7 @@ int main(){
     Beverage* decafOnly = new Decaf();
     cout << decafOnly->getDescription() << endl;
     cout << decafOnly->cost() << endl;
+    delete decafOnly;
     cout << "---------------------------" << endl;
 
     // Decaf + Mocha
@@ -23,6 +24,7 @@ int main(){
     MochaDecorator mocha_decaf = MochaDecorator(decaf);
     cout << mocha_decaf.getDescription() << endl;
     cout << mocha_decaf.cost() << endl;
+    delete decaf;
     cout << "---------------------------" << endl;
 
     // Espresso + Milk
@@ -30,26 +32,30 @@ int main(){
     MilkDecorator milk_espresso = MilkDecorator(espresso);
     cout << milk_espresso.getDescription() << endl;
     cout << milk_espresso.cost() << endl;
-    cout << "---------------------------" << endl;
-
-    // Esresso + Mocha + Milk + Milk
-    // Note: decorators can wrap not only components but the other decorators as well
-    Beverage* espresso2 = new Espresso();                               // Espresso
-    MochaDecorator* mocha_espresso = new MochaDecorator(espresso2);     // Espresso + Mocha
-    MilkDecorator* milk_deco = new MilkDecorator(mocha_espresso);       // Espresso + Mocha + Milk
-    MilkDecorator* milk_deco2 = new MilkDecorator(milk_deco);           // Espresso + Mocha + Milk + Milk
-    cout << milk_deco2->getDescription() << endl;
-    cout << milk_deco2->cost() << endl;
-    cout << "---------------------------" << endl;
-
-
-    delete decafOnly;
-    delete decaf;
     delete espresso;
-    delete espresso2;
-    delete mocha_espresso;
-    delete milk_deco;
-    delete milk_deco2;
+    cout << "---------------------------" << endl;
 
+    // Note: decorators can wrap not only components but the other decorators as well
+
+    // Esresso + Mocha + Milk + Milk,  method 1-------------------------------
+    // Beverage* espresso2 = new Espresso();                               // Espresso
+    // MochaDecorator* mocha_espresso = new MochaDecorator(espresso2);     // Espresso + Mocha
+    // MilkDecorator* milk_deco = new MilkDecorator(mocha_espresso);       // Espresso + Mocha + Milk
+    // MilkDecorator* milk_deco2 = new MilkDecorator(milk_deco);           // Espresso + Mocha + Milk + Milk
+    // cout << milk_deco2->getDescription() << endl;
+    // cout << milk_deco2->cost() << endl;
+    // delete espresso2;
+    // delete mocha_espresso;
+    // delete milk_deco;
+    // delete milk_deco2;
+
+    // Esresso + Mocha + Milk + Milk,  method 2. TODO: how to delete the memory for 
+    Beverage* espresso2 = new MilkDecorator(new MilkDecorator(new MochaDecorator(new Espresso())));
+    cout << espresso2->getDescription() << endl;
+    cout << espresso2->cost() << endl;
+    delete espresso2;
+ 
+    
+    cout << "---------------------------" << endl;
     return 0;
 }
