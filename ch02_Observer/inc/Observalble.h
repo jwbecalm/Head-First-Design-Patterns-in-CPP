c29d7bf6a213f94dd7ba5405409033abcc0027a5
irwin <17735528@qq.com>
2022-10-27 15:47:07 +0800

    update uml link

--- ch02_Observer/inc/Observalble.h
+++ ch02_Observer/inc/Observalble.h
@@ -5,9 +5,9 @@
 
 class Obserbalble{
     public:
-    // 观察者使用该接口注册自己
+    // 观察者使用该接口注册自己（订阅服务）
     virtual void addObserver(Observer* observer) = 0;
-    // 观察者使用该接口删除自己
+    // 观察者使用该接口删除自己（取消订阅）
     virtual void removeObserver(Observer* observer) = 0;
     // 当被观察者某个状态发生变化时，调用该接口通知所有的观察者
     virtual void notifyObservers() = 0;
